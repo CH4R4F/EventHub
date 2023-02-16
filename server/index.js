d8809424554c0268c4b76f69b4c9b55985a28efc
charaf marghin <developermarghin@gmail.com>
2023-02-16 16:27:46 +0100

    add default middlewares

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 // initialize configuration
 const app = express();
 dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
 
 const NODE_ENV = process.env.NODE_ENV || "development";
 
